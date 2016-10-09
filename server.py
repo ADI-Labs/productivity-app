fac25330c10f224d9c150c1158cb24b69effc642
Jonathan <sunyang713@hotmail.com>
2016-10-09 13:08:15 -0400

    Template, base structure.

---
+++ server.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+"""
+Productivity Webserver
+
+To run locally:
+
+    python server.py
+
+Go to http://localhost:8080 in your browser.
+"""
+
+import click
+from app import app
+
+if __name__ == '__main__':
+    @click.command()
+    @click.option('--debug', is_flag=True)
+    @click.option('--threaded', is_flag=True)
+    @click.argument('HOST', default='0.0.0.0')
+    @click.argument('PORT', default=8080, type=int)
+    def serve(debug, threaded, host, port):
+        """
+        Run the server using:
+
+            python server.py
+
+        Show the help text using:
+
+            python server.py --help
+
+        """
+        app.run(host=host, port=port, debug=debug, threaded=threaded)
+
+    serve()
