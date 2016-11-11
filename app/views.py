0e2f3c32ae1fdecef688b09a7b6779d2694e18f3
Lauren Arnett <Lauren@dyn-160-39-9-25.dyn.columbia.edu>
2016-11-11 17:26:05 -0500

    Changed index.html to have to do list

--- app/views.py
+++ app/views.py
@@ -5,3 +5,21 @@ from . import app
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+
+@app.route('/trophies')
+def trophies():
+    return render_template('trophies.jinja', 
+        netflix = "Netflix and Chill",
+        netflix_img = "https://i1.wp.com/fusion.net/wp-content/uploads/2015/08/maxresdefault.jpg?resize=1600%2C900&quality=80&strip=all",
+        socialBee = "Social things",
+        adventure_img = "http://www.infiniteguest.org/wp-content/uploads/2015/06/adventure-time.jpg")
+
+
+
+@app.route('/page2')
+def page2():
+    return render_template('trophies.jinja', 
+        netflix = "Netflix and Chill",
+        socialBee = "Social things")
