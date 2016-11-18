16410c71c8befeb95ee1b5760d2325c06e053f46
Lauren Arnett <lba2138@columbia.edu>
2016-11-18 14:45:08 -0500

    Update views.py
    @aza211

--- app/views.py
+++ app/views.py
@@ -10,16 +10,10 @@ def index():
 
 @app.route('/trophies')
 def trophies():
-    return render_template('trophies.jinja', 
-        netflix = "Netflix and Chill",
-        netflix_img = "https://i1.wp.com/fusion.net/wp-content/uploads/2015/08/maxresdefault.jpg?resize=1600%2C900&quality=80&strip=all",
-        socialBee = "Social things",
-        adventure_img = "http://www.infiniteguest.org/wp-content/uploads/2015/06/adventure-time.jpg")
+    return render_template('trophies.jinja')
 
 
 
 @app.route('/page2')
 def page2():
-    return render_template('trophies.jinja', 
-        netflix = "Netflix and Chill",
-        socialBee = "Social things")
+
